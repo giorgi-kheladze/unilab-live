2025d19a2ed0dfb3dde51cf43e3789c2412e161b
Giorgi Kheladze <157839203+giorgi-kheladze@users.noreply.github.com>
2024-11-01 15:04:11 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const containerP = document.getElementById("container-p")
+const form = document.getElementById("form")
+
+function addItem(){
+    containerP.style.display = "none";
+    form.style.display = "block"
+}
+function clearItem(){
+    containerP.style.display = "block";
+    form.style.display = "none"
+}
+
+const inputText = document.getElementById("input");
+
+let http = new XMLHttpRequest()
+http.open('get', "http://ip-api.com/json/", true)
+http.send()
+
+http.onload = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        let data = JSON.parse(this.responseText);
+        let result = `
+            <p>Country: ${data.country}</p>
+            <p>Timezone: ${data.timezone}</p>
+        `;
+        document.getElementById("form").innerHTML = result;
+    }
+};
